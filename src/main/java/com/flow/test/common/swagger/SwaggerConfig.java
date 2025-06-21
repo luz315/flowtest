d9e4bfb6054fcbf85e1cb1d0780f92143ac78d1d
poorm <315luz10@gmail.com>
2025-06-21 22:25:49 +0900

    [#6][feat] 스웨거 적용

---
+++ src/main/java/com/flow/test/common/swagger/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.flow.test.common.swagger;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("test API")
+                        .description("플로우 과제 API 명세입니다.")
+                        .version("v1"));
+    }
+}
--- src/main/java/com/flow/test/presentation/PostController.java
+++ src/main/java/com/flow/test/presentation/PostController.java
@@ -3,6 +3,9 @@ package com.flow.test.presentation;
 import com.flow.test.application.PostService;
 import com.flow.test.dto.PostCreateRequest;
 import com.flow.test.dto.PostResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -10,11 +13,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/posts")
+@Tag(name = "게시글", description = "게시글 관련 API")
 public class PostController {
 
     private final PostService postService;
 
     @PostMapping
+    @Operation(summary = "게시글 생성", description = "게시글과 파일 확장자 목록을 저장합니다.")
+    @ApiResponse(responseCode = "200", description = "등록 성공")
     public PostResponse createPost(
             @RequestBody @Valid PostCreateRequest request
     ) {
